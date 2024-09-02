792c255cc3ba50cf3867034c4a07dc247ab6ee8f
Mehboob <mehboobarshad300@gmail.com>
2024-09-02 22:25:48 +0500

    Customer Controller updated

--- day_01/Areas/Customer/Controllers/HomeController.cs
+++ day_01/Areas/Customer/Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Web.dataAccess.Repositry;
+using Web.dataAccess.Repositry.IRepositry;
 
 
 namespace day_01.Areas.Customer.Controllers
@@ -10,19 +11,25 @@ namespace day_01.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly UnitOfWork _unitOfWork;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger, UnitOfWork unit)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unit)
         {
             _logger = logger;
-            _unitOfWork = unit;
+           _unitOfWork = unit;
         }
 
         public IActionResult Index()
             
         {
-            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "category");
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"category");
             return View(productList);
+        }
+        public IActionResult Details(int id)
+
+        {
+            Product product = _unitOfWork.Product.Get(u=>u.Id==id,includeProperties: "category");
+            return View(product);
         }
 
         public IActionResult Privacy()
